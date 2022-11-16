0e8849274734c4bab05c24d1155c5f37b1c0dbf9
Fabio Silva <binhudo8@gmail.com>
2022-11-16 11:46:50 -0300

    commit inicial

---
+++ src/serve.js
@@ -0,0 +1,51 @@
+if(process.env.NODE_ENV != 'production'){
+    require('dotenv').config()
+}
+
+
+const express = require('express');
+const GNRequest = require('./apis/grencianet')
+const bodyParser = require('body-parser')
+const app = express()
+
+
+app.set('view engine', 'ejs');
+app.set('views', 'src/views');
+app.use(bodyParser.json())
+const reqGNAlReady =  GNRequest()
+
+app.post('/webhook', (req, res)=> {
+    console.log(req.body)
+    res.send(200)
+})
+
+app.get('/', async (req, res)=> {
+    const reqGN = await reqGNAlReady
+    const dataCob = {
+        "calendario": {
+          "expiracao": 3600
+        },
+        "devedor": {
+          "cpf": "12345678909",
+          "nome": "Francisco da Silva"
+        },
+        "valor": {
+          "original": "1.00"
+        },
+        "chave": "binhudo8@gmail.com",
+        "solicitacaoPagador": "teste pix."
+      }
+
+      
+
+    const cobResponse = await reqGN.post('/v2/cob', dataCob)
+    const qrCodeResponse = await reqGN(`/v2/loc/${cobResponse.data.loc.id}/qrcode`)
+    
+    res.render('qrcode', {qrcodeImage: qrCodeResponse.data.imagemQrcode})
+
+})
+
+
+app.listen(2002, ()=> {
+    console.log('servidor online')
+})
