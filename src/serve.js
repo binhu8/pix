2461392636faaf5f1d0fed82fc5ae2b5704d55c4
Fabio Silva <binhudo8@gmail.com>
2022-11-16 11:52:52 -0300

    certs

--- src/serve.js
+++ src/serve.js
@@ -1,8 +1,5 @@
-if(process.env.NODE_ENV != 'production'){
-    require('dotenv').config()
-}
 
-
+require('dotenv').config()
 const express = require('express');
 const GNRequest = require('./apis/grencianet')
 const bodyParser = require('body-parser')
